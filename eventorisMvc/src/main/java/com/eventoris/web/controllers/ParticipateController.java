ca1dd377d2a6bf654991325c2cce489703d033ff
Viorrika <viorika.t@gmail.com>
2014-06-15 15:18:57 +0300

    participantInfo

---
+++ eventorisMvc/src/main/java/com/eventoris/web/controllers/ParticipateController.java
@@ -0,0 +1,38 @@
+package com.eventoris.web.controllers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventoris.service.EventManager;
+
+@Controller
+@RequestMapping(value = "/comeforsure**")
+public class ParticipateController {
+
+	/** Logger for this class and subclasses */
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	private EventManager eventManager;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String processSubmit(
+			@ModelAttribute("commentData") CommentData commentData,
+			BindingResult result, Model model) {
+		ModelAndView mv = new ModelAndView();
+		logger.info("AddParticipant: Status=going;  ");
+		mv.addObject("commentData", commentData);
+		return "redirect:eventdetail.htm?event=" + commentData.getEventid();
+
+	}
+
+	public void setEventManager(EventManager eventManager) {
+		this.eventManager = eventManager;
+	}
+}
